6c4b19ada2f90f76ac3bca13514253ba721af5a9
RonakAgarwal <agarwalronak02@gmail.com>
2018-07-19 19:16:00 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,33 +2,33 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name
-        age
+        name: "Lady Montague",
+        age: "53"
         },
       :matriarch => {
-        name
-        age
+        name: "Lady Montague",
+        age: "53"
         },
       :hero => {
-        name
-        age
-        status: alive
+        name: "Romeo",
+        age: "15",
+        status: "alive"
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
         name: "Lord Capulet",
-        age: 50
+        age: "50"
         },
       :matriarch => {
         name: "Lady Capulet",
-        age:51
+        age:"51"
         },
       :heroine => {
-        name: Juliet
-        age: 15
-        status: alive
+        name: "Juliet"
+        age: "15"
+        status: "alive"
         },
       :heroine_friends => []
    }
