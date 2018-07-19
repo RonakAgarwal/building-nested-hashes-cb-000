fbd77bf3ec7604f82fabc335c4ccd441f40dea99
RonakAgarwal <agarwalronak02@gmail.com>
2018-07-19 19:14:42 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,12 +18,12 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet"
-        age:
+        name: "Lord Capulet",
+        age: 50
         },
       :matriarch => {
-        name: 
-        age:
+        name: "Lady Capulet",
+        age:51
         },
       :heroine => {
         name: Juliet
