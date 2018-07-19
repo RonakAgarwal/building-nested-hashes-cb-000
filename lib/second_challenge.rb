eb8b375046c45c6b30d2d9b8d6e76bc61d3d1123
RonakAgarwal <agarwalronak02@gmail.com>
2018-07-19 19:09:38 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,29 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-       :patriarch => {
-         #your key/value pairs here
-         },
-       :matriarch => {
-         #your key/value pairs here
-         },
-       :hero => {
-         #your key/value pairs here
-         },
-       :hero_friends => []
-    },
+      :patriatch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => {}
+      },
     :capulet => {
-       :patriarch => {
-         #your key/value pairs here
-         },
-       :matriarch => {
-         #your key/value pairs here
-         },
-       :heroine => {
-         #your key/value pairs here
-         },
-       :heroine_friends => []
-    }
+      :patriatch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => {}
+      }
     }
 
 
