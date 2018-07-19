a05e8d3574a4bce305638aa05343d1bca533e9b1
RonakAgarwal <agarwalronak02@gmail.com>
2018-07-19 19:10:28 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,27 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriatch => {},
-      :matriarch => {},
-      :hero => {},
+      :patriatch => {
+        
+      },
+      :matriarch => {
+        
+      },
+      :hero => {
+        
+      },
       :hero_friends => []
       },
     :capulet => {
-      :patriatch => {},
-      :matriarch => {},
-      :heroine => {},
+      :patriatch => {
+        
+      },
+      :matriarch => {
+        
+      },
+      :heroine => {
+        
+      },
       :heroine_friends => []
       }
     }
